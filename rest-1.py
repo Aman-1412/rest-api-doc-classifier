09e9bee296545eb362653cf9d6448d8783c48516
Aman Goyal <amangoyal1412@gmail.com>
2017-08-01 18:57:23 +0530

    Backward compatibility

--- rest-1.py
+++ rest-1.py
@@ -3,24 +3,18 @@ from classifier import train_svm, test_svm
 app = Flask(__name__)
 
 
-# classifier="clf_svm"
-# classifier = "clf_rf"
-# saved_model_path="/path/to/uploaded model"
-# train_ratio = 0.8
-# test_ratio = 1 - train_ratio
-# test_document_path=""
-# test_document_raw=""
+#Declare some properties
 
 default_train_dir_path = "D:\kaam\AdditionalParsed"
 default_test_dir_path = r"D:\kaam\AdditionalParsedTest"
 
 @app.route('/document-classifier/api/v1/train', methods = ['GET','POST'])
 def trainme():
-	# classifier abhi mat use karna
-	# classifier = request.args.get("classifier") if request.args.get("classifier") else "asdfg"
+
 	dirPath = request.args.get("train_path") or default_train_dir_path
 	train_ratio = float(request.args.get("train_ratio")) if request.args.get("train_ratio") else  0.8
-	test_ratio = float(f'{1 - train_ratio:.4f}')
+	# test_ratio = float(f'{1 - train_ratio:.4f}')
+	test_ratio = round(1 - train_ratio,2)
 	print("Values aa gayi(GET)")
 	params = {"dirPath" : dirPath,"train_ratio": train_ratio,"kernel_provided" : "rbf", "C_provided": 10000.0,"gamma_provided" : 0.6}
 	print("bhej raha hu")
@@ -28,8 +22,6 @@ def trainme():
 	print(modelFilePath+ " bhangar wala\n")
 	modelFilePath = modelFilePath.replace("\\",'/')
 	print(modelFilePath+ " result aaya")
-	# if not modelFilePath:
-	# 	return redirect(url_for('nomasti'))
 	return (jsonify({"modelStoredAt" : modelFilePath}))
 
 @app.route('/document-classifier/api/v1/test', methods = ['GET','POST'])
@@ -52,16 +44,6 @@ def nomasti():
 @app.route('/')
 def index():
     return("Hola! Secret api services available here on request")
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/api/v1/fornow', methods=['GET'])
-def fornow():
-    return jsonify({"the": "proof is in the papers"})
-
-
 
 
 @app.errorhandler(404)
